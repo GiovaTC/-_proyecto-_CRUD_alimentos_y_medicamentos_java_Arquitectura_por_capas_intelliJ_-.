fc347f06bc989d7190a723f41e05e00f86151e6c
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-20 10:52:46 -0500

    commit - Alimento : .

--- src/main/java/com/example/crudalimentosmedicamentos/crud/model/Alimento.java
+++ src/main/java/com/example/crudalimentosmedicamentos/crud/model/Alimento.java
@@ -1,4 +1,26 @@
 package com.example.crudalimentosmedicamentos.crud.model;
 
+import java.time.LocalDate;
+
 public class Alimento {
+    private Long id;
+    private String nombre;
+    private String categoria;
+    private LocalDate fechaVencimiento;
+    private Integer stock;
+    private String observaciones;
+
+    // getters y setters
+    public Long getId() { return id;}
+    public void setId(Long id) { this.id = id;}
+    public String getNombre() { return nombre;}
+    public void setNombre(String nombre) { this.nombre = nombre;}
+    public String getCategoria() { return categoria;}
+    public void setCategoria(String categoria) { this.categoria = categoria;}
+    public LocalDate getFechaVencimiento() { return fechaVencimiento;}
+    public void setFechaVencimiento(LocalDate fechaVencimiento) { this.fechaVencimiento = fechaVencimiento;}
+    public Integer getStock() { return stock;}
+    public void setStock(Integer stock) { this.stock = stock;}
+    public String getObservaciones() { return observaciones;}
+    public void setObservaciones(String observaciones) { this.observaciones = observaciones;}
 }

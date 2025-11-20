7a6a5ca1a3742aa149493cd41e4553258a0391da
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-20 15:35:44 -0500

    commit - AlimentoService .

--- src/main/java/com/example/crudalimentosmedicamentos/crud/service/AlimentoService.java
+++ src/main/java/com/example/crudalimentosmedicamentos/crud/service/AlimentoService.java
@@ -1,4 +1,17 @@
 package com.example.crudalimentosmedicamentos.crud.service;
 
+import com.example.crudalimentosmedicamentos.crud.dao.MedicamentoDAO;
+import com.example.crudalimentosmedicamentos.crud.dao.MedicamentoDAOImpl;
+import com.example.crudalimentosmedicamentos.crud.model.Medicamento;
+
+import java.util.List;
+
 public class AlimentoService {
+    private final MedicamentoDAO dao = new MedicamentoDAOImpl();
+
+    public List<Medicamento> listar() throws Exception{ return dao.findAll();}
+    public Medicamento obtener(Long id) throws Exception{ return dao.findById(id);}
+    public void crear(Medicamento m) throws  Exception { dao.insert(m); }
+    public void actualizar(Medicamento m) throws Exception{ dao.update(m); }
+    public void eliminar(Long id) throws Exception{ dao.delete(id);}
 }

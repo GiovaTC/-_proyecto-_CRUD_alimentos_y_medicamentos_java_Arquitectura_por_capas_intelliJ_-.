46834174d1cd6749470a54649a8ebd22004adbd2
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-20 11:06:39 -0500

    commit - Medicamento .

--- src/main/java/com/example/crudalimentosmedicamentos/crud/model/Medicamento.java
+++ src/main/java/com/example/crudalimentosmedicamentos/crud/model/Medicamento.java
@@ -1,4 +1,71 @@
 package com.example.crudalimentosmedicamentos.crud.model;
 
+import java.time.LocalDate;
+
 public class Medicamento {
+    private Long id;
+    private String nombre;
+    private String principioActivo;
+    private String dosis;
+    private LocalDate fechaVencimiento;
+    private Integer stock;
+    private String observaciones;
+
+    // getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrincipioActivo() {
+        return principioActivo;
+    }
+
+    public void setPrincipioActivo(String principioActivo) {
+        this.principioActivo = principioActivo;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public LocalDate getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(LocalDate fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
 }

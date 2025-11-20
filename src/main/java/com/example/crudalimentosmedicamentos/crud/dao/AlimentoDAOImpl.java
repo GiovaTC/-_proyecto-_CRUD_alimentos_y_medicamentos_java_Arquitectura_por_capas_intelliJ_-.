ccfce515db65d5457cbe371adaca8b95daf448ad
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-20 12:11:53 -0500

    commit - AlimentoDAOImpl .

--- src/main/java/com/example/crudalimentosmedicamentos/crud/dao/AlimentoDAOImpl.java
+++ src/main/java/com/example/crudalimentosmedicamentos/crud/dao/AlimentoDAOImpl.java
@@ -39,7 +39,33 @@ public class AlimentoDAOImpl implements AlimentoDAO {
            ps.setString(1, a.getNombre());
            ps.setString(2, a.getCategoria());
            ps.setDate(3, a.getFechaVencimiento() == null ? null : Date.valueOf(a.getFechaVencimiento()));
-        }   
+           ps.setObject(4, a.getStock(), Types.INTEGER);
+           ps.setString(5, a.getObservaciones());
+           ps.executeUpdate();
+        }
+    }
+
+    @Override
+    public void update(Alimento a) throws Exception {
+        String sql = "UPDATE ALIMENTOS SET NOMBRE=?, CATEGORIA=?, FECHA_VENCIMIENTO=?, STOCK=?, OBSERVACIONES=? WHERE ID=?";
+        try (Connection c = DbUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql)) {
+            ps.setString(1, a.getNombre());
+            ps.setString(2, a.getCategoria());
+            ps.setDate(3, a.getFechaVencimiento() == null ? null : Date.valueOf(a.getFechaVencimiento()));
+            ps.setObject(4, a.getStock(), Types.INTEGER);
+            ps.setString(5, a.getObservaciones());
+            ps.setLong(6, a.getId());
+            ps.executeUpdate();
+        }
+    }
+
+    @Override
+    public void delete(Long id) throws Exception {
+        try (Connection c = DbUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement("DELETE FROM ALIMENTOS WHERE ID = ?")) {
+             ps.setLong(1, id);
+             ps.executeUpdate();
+        }
     }
 
     private Alimento map(ResultSet rs) throws SQLException {
@@ -50,7 +76,7 @@ public class AlimentoDAOImpl implements AlimentoDAO {
         Date d = rs.getDate("FECHA_VENCIMIENTO");
         if (d != null) a.setFechaVencimiento(d.toLocalDate());
         a.setStock((Integer) rs.getObject("STOCK"));
-        a.setObservaciones(rs.getString("OBSERVACION"));
+        a.setObservaciones(rs.getString("OBSERVACIONES"));
         return a;
     }
 }
